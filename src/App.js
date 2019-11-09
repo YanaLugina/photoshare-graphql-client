d9cff59e926d28884da6f0da7975201b9986cea8
YanaLugina <yana4961@gmail.com>
2019-11-09 23:50:05 +1000

    clientCreate: created client, received data from the server, made a button to synchronize with the server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Users from './Users';
+import { gql } from 'apollo-boost';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const ROOT_QUERY = gql `
+    query allUsers {
+      totalUsers
+      allUsers {
+        githubLogin
+        name
+        avatar
+      }
+    }
+`;
+
+const App = () => {
+  return (<Users />);
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient, { gql } from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
 
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql'
 });
-
-const query = gql`
-    {
-        totalUsers
-        totalPhotos
-    }
-`;
-
-client.query({ query })
-    .then(({ data }) => console.log('data', data))
-    .catch(console.error);
 
 render (
     <ApolloProvider client={client}>
