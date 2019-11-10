70ce3552042c332fd9e04343c7d0bf6227ad473c
YanaLugina <yana4961@gmail.com>
2019-11-11 01:04:29 +1000

    authorization: created authorization for user: created AuthorizedUser, button for authorized and wrap App children with BrowserRouter from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import Users from './Users';
+import { BrowserRouter } from 'react-router-dom';
 import { gql } from 'apollo-boost';
+import AuthorizedUser from './AuthorizedUser';
+
 import './App.css';
 
 export const ROOT_QUERY = gql `
     query allUsers {
       totalUsers
-      allUsers {
+      allUsers { ...userInfo }
+      me { ...userInfo }
+    }
+    
+    fragment userInfo on User {
         githubLogin
         name
         avatar
-      }
     }
 `;
 
 const App = () => {
-  return (<Users />);
+  return (
+          <BrowserRouter>
+              <div>
+                  <AuthorizedUser />
+                  <Users />
+              </div>
+          </BrowserRouter>
+      );
 };
 
 export default App;
